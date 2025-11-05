76c3a1b100c02bb26b5059335c6f58b7145f6724
steevenmondithoka <steevemondithoka@gmail.com>
2025-11-05 22:37:44 +0530

    steeven

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
         setError('');
 
         try {
-           const res = await api.post('/login', formData);
+           const res = await api.post('/api/auth/login', formData);
 
 
 
