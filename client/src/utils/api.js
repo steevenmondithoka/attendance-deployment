271ad924d6c8b90e7252e628ccdc88ebb04443cd
steevenmondithoka <steevemondithoka@gmail.com>
2025-11-05 20:56:15 +0530

    steeven

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://attendance-deployment.onrender.com';
 
 const api = axios.create({
     baseURL: BACKEND_URL,
