44ea4dbec1f3c57681b3db0a6069d73392eaa81f
steevenmondithoka <steevemondithoka@gmail.com>
2025-11-06 08:50:36 +0530

    steeven

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,17 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { ThemeProvider } from './components/ThemeProvider';
+import { SocketProvider } from './context/SocketContext';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   
     <BrowserRouter>
-   
+    <SocketProvider>
      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <App />
       </ThemeProvider>
-  
+    </SocketProvider>
     </BrowserRouter>
    
   </React.StrictMode>
