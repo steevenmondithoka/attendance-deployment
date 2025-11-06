aa1be42767b684984055285d8c46f8adf231d294
steevenmondithoka <steevemondithoka@gmail.com>
2025-11-06 12:27:13 +0530

    steeven

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
                     <Routes>
                      <Route path="/" element={<Home />} /> 
                         <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
+                       
 
                         <Route
                             path="/teacher-dashboard"
